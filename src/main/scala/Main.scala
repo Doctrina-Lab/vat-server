c52d547e9847e05ed5d5de27f69b630715fdaec9
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-16 14:24:52 +0200

    Return a JSON object instead of just a double value

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,8 +4,8 @@ import zio.http._
 object VatServer extends ZIOAppDefault:
   val app: HttpApp[Any] = 
     Routes(
-      Method.GET / "germany" -> handler(Response.text(f"${19.0}%.2f")),
-      Method.GET / "netherlands" -> handler(Response.text(f"${21.0}%.2f"))
+      Method.GET / "germany" -> handler(Response.json("""{"vat": 19.0}""")),
+      Method.GET / "netherlands" -> handler(Response.json("""{"vat": 21.0}"""))
     ).toHttpApp
   
   override val run = 
